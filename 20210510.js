901f34b42b1bf9b3948a6c9a3f7d262b4f658a1a
Mark Stickling <mkstix6@gmail.com>
2021-05-10 21:55:26 +0100

    First version submitted for challenge 195 10th May 2021

---
+++ 20210510.js
@@ -0,0 +1,34 @@
+/**
+ * Given an integer n, return true if n^3 and n
+ * have the same set of digits.
+ *
+ * Example:
+ * $ sameDigits(1) // true
+ * $ sameDigits(10) // true
+ * $ sameDigits(251894) // true
+ * $ sameDigits(251895) // false
+ */
+
+const sameDigitsVerbose = (number) => {
+  const numberDigits = new Set(number.toString());
+  const result = Math.pow(number, 3);
+  const resultDigits = new Set([...numberDigits, ...result.toString()]);
+  console.log(
+    { number, result, numberDigits, resultDigits },
+    numberDigits.size
+  );
+  return numberDigits.size === resultDigits.size;
+};
+
+const sameDigits = (number) =>
+  new Set(number.toString()).size ===
+  new Set(number.toString() + (number ** 3).toString()).size;
+
+const tests = [
+  [1, true],
+  [10, true],
+  [251894, true],
+  [251895, false],
+].forEach(([number, expected]) => {
+  console.log(sameDigits(number) === expected ? "✅ passing" : "❌ failing");
+});
