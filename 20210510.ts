a7876762790d77b4458511fd1662fcabeafd4e88
Mark Stickling <mkstix6@gmail.com>
2021-05-11 00:04:57 +0100

    TypeScript version for challenge #195

---
+++ 20210510.ts
@@ -0,0 +1,27 @@
+/**
+ * Given an integer n, return true if n^3 and n
+ * have the same set of digits.
+ *
+ * Example:
+ * $ sameDigits(1) // true
+ * $ sameDigits(10) // true
+ * $ sameDigits(251894) // true
+ * $ sameDigits(251895) // false
+ */
+
+const digitString = (n: number): string => [...new Set(`${n}`)].sort().join("");
+const sameDigits = (a: number, b: number): boolean =>
+  digitString(a) === digitString(b);
+const sameDigitsAsCube = (n: number): boolean => sameDigits(n, n ** 3);
+
+const tests: [number, boolean][] = [
+  [1, true],
+  [10, true],
+  [251894, true],
+  [251895, false],
+  [106239, false],
+];
+
+tests.forEach(([n, expected]: [number, boolean]) => {
+  console.log(sameDigitsAsCube(n) === expected ? "✅ passing" : "❌ failing");
+});
